650312127071020cd5ce132afa447f48cdae6aff
Dégi János <johann.degi@gmail.com>
2018-09-10 15:59:41 +0200

    add GreetingRepository

---
+++ src/main/java/guru/springframework/repositories/GreetingRepository.java
@@ -0,0 +1,12 @@
+package guru.springframework.repositories;
+
+/**
+ * Created by Dégi János on 2018.09.10..
+ */
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+    String getSpanishGreeting();
+    String getGermanGreeting();
+
+}
