9534a5a955c8ef96a64b029f4e53080e0772f5fe
Dégi János <johann.degi@gmail.com>
2018-09-11 10:32:05 +0200

    Bean LifeCycles

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # springframework_di_demo
 guru.springframework
+
+GIT
+
+ERRORS:
+
+1
+Push rejected: Push to origin/master was rejected
+Pull.. (there is no versioned file in the remote repo - Readme.md)
+
+2
+Git Pull Failed: refusing to merge unrelated histories
+
+solution:
+Terminal (command line)
+git pull origin master --allow-unrelated-histories
+
--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,8 +1,6 @@
 package guru.springframework;
 
 import guru.springframework.controllers.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
