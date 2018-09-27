cd9342ec713a86a0daf217537e731726e6ad273d
Dégi János <johann.degi@gmail.com>
2018-09-27 12:56:17 +0200

    creating a unit test

---
+++ src/test/java/guru/springframework/examplebeans/SpringBootApplicationPropertyDataSourceTest.java
@@ -0,0 +1,39 @@
+package guru.springframework.examplebeans;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jd on 2018.09.27..
+ */
+public class SpringBootApplicationPropertyDataSourceTest {
+
+    SpringBootApplicationPropertyDataSource springBootAppPropDataSource;
+
+    @Before
+    public void setUp() {
+        springBootAppPropDataSource = new SpringBootApplicationPropertyDataSource();
+    }
+
+    @Test
+    public void getApplicationName() throws Exception {
+        String appName = "di-demo";
+
+        springBootAppPropDataSource.setApplicationName("sss");
+
+        assertEquals("test msg", appName, springBootAppPropDataSource.getApplicationName() );
+    }
+
+    @Test
+    public void getApplicationUrl() throws Exception {
+
+    }
+
+    @Test
+    public void getApplicationUser() throws Exception {
+
+    }
+
+}
